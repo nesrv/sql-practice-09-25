f7064cc1bbee2f5dbaf30f875cd30229b9d99830
Student <student@example.com>
2025-09-04 09:43:38 +0300

    5

--- 5.sql
+++ 5.sql
@@ -2,3 +2,27 @@
 UPDATE products 
 SET is_active = FALSE
 WHERE category_id = (SELECT id FROM categories WHERE name = 'Книги');
+
+
+
+-- Увеличьте цены всех товаров в категории "Спорт" на 15%
+UPDATE products 
+SET price = price*1.15
+WHERE category_id = (SELECT id FROM categories WHERE name = 'Спорт');
+
+UPDATE customers 
+SET email = 'ivan.petrov@newmail.com' 
+WHERE first_name = 'Иван' AND last_name = 'Петров';
+
+-- Удалить по условию
+DELETE FROM products WHERE stock_quantity <  30;
+
+
+DELETE FROM products 
+WHERE category_id = (SELECT id FROM categories WHERE name = 'Устаревшие');
+
+
+DELETE FROM customers WHERE email IS NULL;
+
+
+-- \copy products TO 'products_export.csv' CSV HEADER;
