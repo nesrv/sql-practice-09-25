c39c17c77fe5f1ef508e3311a2ad2c95b1f3df2a
Student <student@example.com>
2025-09-04 11:04:15 +0300

    11+

--- 11.sql
+++ 11.sql
@@ -25,3 +25,45 @@ INSERT INTO marketing_employees VALUES
 (5, 'Дмитрий', 'Волков', 'Analytics'),
 (6, 'Анна', 'Новикова', 'Design'),
 (7, 'Петр', 'Иванов', 'SMM');
+
+select * from it_employees
+UNION
+select * from marketing_employees;
+
+select * from it_employees
+UNION all
+select * from marketing_employees;
+
+select * from it_employees
+EXCEPT
+select * from marketing_employees;
+
+
+select skill from it_employees
+EXCEPT
+select skill from marketing_employees;
+
+
+
+select skill from it_employees
+INTERSECT
+select skill from marketing_employees;
+
+
+-- Сравнение навыков между отделами
+
+SELECT 'Только ИТ' as category, skill FROM it_employees
+EXCEPT
+SELECT 'Только ИТ', skill FROM marketing_employees;
+
+
+-- SELECT first_name, last_name FROM it_employees
+--     UNION
+-- SELECT first_name, last_name FROM marketing_employees;
+
+SELECT 'Всего уникальных' as metric, COUNT(*) as count FROM (
+    SELECT first_name, last_name FROM it_employees
+    UNION
+    SELECT first_name, last_name FROM marketing_employees
+) t;
+
