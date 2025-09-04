e144149b3497cdf5ac5c5f9119856f1cd4511a7e
Student <student@example.com>
2025-09-04 10:38:17 +0300

    +

---
+++ 10.sql
@@ -0,0 +1,27 @@
+SELECT
+    e.last_name,  
+    p.project_name,
+    d.department_name   
+FROM employees e
+NATURAL JOIN  projects p 
+NATURAL JOIN  departments d;
+
+
+SELECT
+    d.department_name, 
+    count(e.last_name)      
+FROM employees e
+NATURAL JOIN  projects p 
+NATURAL JOIN  departments d
+GROUP by d.department_name;
+
+
+SELECT
+    d.department_name,
+    p.project_name, 
+    count(e.last_name)      
+FROM employees e
+NATURAL JOIN  projects p 
+NATURAL JOIN  departments d
+GROUP by d.department_name, p.project_name;
+
