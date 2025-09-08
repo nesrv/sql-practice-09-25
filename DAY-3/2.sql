d43f160c7ab2d6210c546761d15d5a5d18e28ef7
Student <student@example.com>
2025-09-08 09:22:37 +0300

    2+

--- DAY-3/2.sql
+++ DAY-3/2.sql
@@ -3,3 +3,13 @@ from sales;
 
 SELECT seller, sale_date,amount, LAG(amount, 1, 0) OVER (PARTITION by seller ORDER BY sale_date)
 from sales;
+
+SELECT 
+    seller,
+    sale_date,
+    amount, 
+    LAG(amount, 1, 0) OVER w AS previous_amount,
+    amount - LAG(amount) OVER w AS difference,
+    ROUND((amount - LAG(amount) OVER w) /  LAG(amount) OVER (PARTITION BY seller ORDER BY sale_date) * 100, 2) AS growth_percent
+from sales
+window w as (PARTITION by seller ORDER BY sale_date);
