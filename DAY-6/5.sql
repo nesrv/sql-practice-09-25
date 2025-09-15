4fa2c90dbbd393be315d377182434fea9cadd625
Student <student@example.com>
2025-09-15 09:47:57 +0300

    5-5

---
+++ DAY-6/5.sql
@@ -0,0 +1,14 @@
+LOOP
+    -- операторы
+END LOOP;
+
+
+FOR имя IN низ .. верх BY инкремент
+LOOP
+    -- операторы
+END LOOP;
+
+FOR имя IN REVERSE верх .. низ BY инкремент
+LOOP
+    -- операторы
+END LOOP;
