49fc9738445c345e33a0a80d6fbd8fbf262bf94b
Student <student@example.com>
2025-09-16 11:29:02 +0300

    6-19+

--- DAY-6/19.sql
+++ DAY-6/19.sql
@@ -1,3 +1,5 @@
+-- # Правила RULES
+
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -12,3 +14,29 @@ INSERT INTO employees (name, department, salary) VALUES
 ('Кузнецова Елена Дмитриевна', 'Маркетинг', 78000.00),
 ('Федорова Мария Петровна', 'Бухгалтерия', 87000.50),
 ('Николаев Андрей Игоревич', 'IT-отдел', 135000.00);
+
+CREATE VIEW marketing_employees AS
+SELECT id, name, salary FROM employees WHERE department = 'Маркетинг';
+
+
+select * from marketing_employees;
+
+CREATE RULE insert_marketing AS ON INSERT TO marketing_employees DO INSTEAD
+INSERT INTO employees (name, department, salary) VALUES (NEW.name, 'Маркетинг', NEW.salary);
+
+CREATE RULE update_marketing AS ON UPDATE TO marketing_employees DO INSTEAD
+UPDATE employees SET name = NEW.name, salary = NEW.salary 
+WHERE id = OLD.id AND department = 'Маркетинг';
+
+CREATE RULE delete_marketing AS ON DELETE TO marketing_employees DO INSTEAD
+DELETE FROM employees WHERE id = OLD.id AND department = 'Маркетинг';
+
+
+INSERT INTO marketing_employees (name, salary) VALUES ('Джон Бидон', 50000);
+
+
+
+
+
+
+
