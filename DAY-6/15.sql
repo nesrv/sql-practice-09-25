5db61c8389d0da10ce259d0ba977a615f2b99d03
Student <student@example.com>
2025-09-16 10:55:26 +0300

    6-16+

--- DAY-6/15.sql
+++ DAY-6/15.sql
@@ -12,3 +12,28 @@ BEGIN
 		RAISE NOTICE 'Оператор SELECT INTO выполнился, n = %', n;
 END
 $$;
+
+
+CREATE TABLE t(id integer);
+INSERT INTO t(id) VALUES (1);
+
+INSERT INTO t(id) VALUES (2);
+
+
+DO $$
+DECLARE
+    n integer;
+BEGIN
+	n := 1/0;
+SELECT id INTO STRICT n FROM t;
+	RAISE NOTICE 'Оператор SELECT INTO выполнился, n = %', n;
+EXCEPTION
+ 	 WHEN TOO_MANY_ROWS THEN
+	 	RAISE NOTICE 'Слишком много данных';
+		RAISE NOTICE 'Строк в таблице: %, n = %', (SELECT count(*) FROM t), n; 
+	 WHEN no_data_found THEN
+        RAISE NOTICE 'Нет данных';
+	WHEN division_by_zero THEN
+        RAISE NOTICE 'Деление на ноль';
+END
+$$;
