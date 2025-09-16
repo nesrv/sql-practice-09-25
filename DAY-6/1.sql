756b369ff3d4e80139c444476b25c4e1380d589b
Student <student@example.com>
2025-09-16 09:03:50 +0300

    6-1

--- DAY-6/1.sql
+++ DAY-6/1.sql
@@ -1,16 +1,7 @@
-DO $$
-DECLARE
- 	-- сами операторы могут и отсутствовать
-	foo integer NOT NULL := 0;
-	bar CONSTANT text := 42;
-	
+ DO $$
+ DECLARE
+ 	cmd CONSTANT text := 'CREATE TABLE city_msk(name text, architect text, founded integer)';
 BEGIN
-	/* А это — многострочный.
-       После каждого объявления ставится знак ';'.
-       Этот же знак ставится после каждого оператора.
-    */
-   -- bar := bar + 1;
-   RAISE NOTICE '%, %', foo, bar;
-	
+    EXECUTE cmd; -- таблица для исторических зданий Москвы
 END
 $$;
