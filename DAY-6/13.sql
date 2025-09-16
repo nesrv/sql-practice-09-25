be2b30dcd0f6a3fcd394f9317cfbddf158c9bece
Student <student@example.com>
2025-09-16 10:20:22 +0300

    6-13+

--- DAY-6/13.sql
+++ DAY-6/13.sql
@@ -13,3 +13,16 @@ CREATE TABLE posts_tags(
     post_id integer REFERENCES posts(post_id),
     tag_id integer REFERENCES tags(tag_id)
 );
+
+
+
+INSERT INTO posts(post_id,message) VALUES
+    (1, 'Перечитывал пейджер, много думал.'),
+    (2, 'Это было уже весной и я отнес елку обратно.');
+
+
+INSERT INTO tags(tag_id,name) VALUES
+    (1, 'былое и думы'), (2, 'технологии'), (3, 'семья');
+
+INSERT INTO posts_tags(post_id,tag_id) VALUES
+    (1,1), (1,2), (2,1), (2,3);
